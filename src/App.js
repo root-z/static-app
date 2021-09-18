ea25b31c3049e0d21813e0ace1958765b3b1676f
root-z <zane.z.lin@gmail.com>
2021-09-18 06:18:23 -0700

    Testing application update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Testing react app on github pages.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
